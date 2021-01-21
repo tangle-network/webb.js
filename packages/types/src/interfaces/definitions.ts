fcd0748125021b5079f23391f09cfa0eb4432a53
Drew Stone <drewstone329@gmail.com>
2021-01-21 14:12:45 -0500

    Add stubs

--- packages/types/src/interfaces/definitions.ts
+++ packages/types/src/interfaces/definitions.ts
@@ -1,14 +1,2 @@
-export { default as accounts } from './accounts/definitions';
-export { default as auctionManager } from './auctionManager/definitions';
-export { default as cdpEngine } from './cdpEngine/definitions';
-export { default as dex } from './dex/definitions';
-export { default as evm } from './evm/definitions';
-export { default as homa } from './homa/definitions';
-export { default as incentives } from './incentives/definitions';
-export { default as loans } from './loans/definitions';
-export { default as nft } from './nft/definitions';
-export { default as nomineesElection } from './nomineesElection/definitions';
-export { default as primitives } from './primitives/definitions';
+export { default as accounts } from './mixer/definitions';
 export { default as runtime } from './runtime/definitions';
-export { default as stakingPool } from './stakingPool/definitions';
-export { default as support } from './support/definitions';
