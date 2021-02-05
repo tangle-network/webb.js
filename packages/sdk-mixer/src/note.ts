f6b08b6976993d873e25f4c4e4827901bd0f2b30
Shady Khalifa <shekohex@gmail.com>
2021-02-05 22:37:45 +0000

    fix note format issue.

--- packages/sdk-mixer/src/note.ts
+++ packages/sdk-mixer/src/note.ts
@@ -1,5 +1,5 @@
 import { hexToU8a, u8aToHex } from '@polkadot/util';
-import { WebbNotePrefix, TokenSymbol, Scalar, Asset } from '@webb-tools/sdk-mixer';
+import { WebbNotePrefix, TokenSymbol, Asset } from '@webb-tools/sdk-mixer';
 
 export class Note {
   // Default constructor
@@ -10,13 +10,12 @@ export class Note {
     public readonly id: number,
     // can be set later after the TX.
     public blockNumber: number | null,
-    public readonly r: Scalar,
-    public readonly nullifier: Scalar
+    public readonly rAndNullifier: Uint8Array
   ) {}
 
   public static deserialize(value: string): Note {
     const parts = value.split('-');
-    const withBlockNumber = parts.length === 6;
+    const withBlockNumber = parts.length === 5;
     let i = -1;
     const prefix = parts[++i] as WebbNotePrefix; // 0
     const version = parts[++i]; // 1
@@ -26,10 +25,9 @@ export class Note {
     if (withBlockNumber) {
       blockNumber = parseInt(parts[++i]); // 4?
     }
-    const r = hexToU8a('0x' + parts[++i]); // 4-5
-    const nullifier = hexToU8a('0x' + parts[++i]); // 5-6
+    const rAndNullifier = hexToU8a('0x' + parts[++i]); // 4-5
     // assign values here after parsing it.
-    return new Note(prefix, version, tokenSymbol, id, blockNumber, r, nullifier);
+    return new Note(prefix, version, tokenSymbol, id, blockNumber, rAndNullifier);
   }
 
   public serialize(): string {
@@ -41,8 +39,7 @@ export class Note {
     if (this.blockNumber !== null) {
       parts.push(this.blockNumber.toString());
     }
-    parts.push(u8aToHex(this.r, -1, false));
-    parts.push(u8aToHex(this.nullifier, -1, false));
+    parts.push(u8aToHex(this.rAndNullifier, -1, false));
 
     const final = parts.join('-');
     return final;
