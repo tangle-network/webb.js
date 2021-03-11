b51c69c1cda6d919244b23702e5a7d137809f35b
Shady Khalifa <shekohex@gmail.com>
2021-03-11 18:59:24 +0200

    Add Rust CI (#55)
    Co-authored-by: Shady Khalifa <shekohex@gmail.com>
    Co-authored-by: Drew Stone <drewstone329@gmail.com>

--- packages/wasm-utils/src/lib.rs
+++ packages/wasm-utils/src/lib.rs
@@ -12,13 +12,13 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+	// This provides better error messages in debug mode.
+	// It's disabled in release mode so it doesn't bloat up the file size.
+	#[cfg(debug_assertions)]
+	console_error_panic_hook::set_once();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+	// Your code goes here!
+	console::log_1(&JsValue::from_str("Hello world!"));
 
-    Ok(())
+	Ok(())
 }
