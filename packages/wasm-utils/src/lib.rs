ee2a4476b80cb259458bf607df7d7f54b17f84bb
Shady Khalifa <shekohex@gmail.com>
2021-03-25 16:57:47 +0200

    rename push_leaf to add_leaf

--- packages/wasm-utils/src/lib.rs
+++ packages/wasm-utils/src/lib.rs
@@ -422,7 +422,7 @@ impl MerkleTree {
 		}
 	}
 
-	pub fn push_leaf(&mut self, index: u64, leaf: Uint8Array) -> Result<(), JsValue> {
+	pub fn add_leaf(&mut self, index: u64, leaf: Uint8Array) -> Result<(), JsValue> {
 		let idx = Scalar::from(index);
 		let leaf = ScalarWrapper::try_from(leaf)?;
 		self.inner.tree.update(idx, *leaf);
