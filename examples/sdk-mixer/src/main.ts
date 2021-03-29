b9516d0ef2125702ead513127b07ebf45fe85169
Shady Khalifa <shekohex@gmail.com>
2021-03-29 18:53:56 +0200

    add deposit example

--- examples/sdk-mixer/src/main.ts
+++ examples/sdk-mixer/src/main.ts
@@ -1,12 +1,47 @@
-// @ts-ignore
-import Worker from './mixer.worker.ts';
-// @ts-ignore
-import { Mixer, MixerAssetGroup } from '@webb-tools/sdk-mixer';
+import { options } from '@webb-tools/api';
+import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
+import { LoggerService } from '@webb-tools/app-util';
+import { Mixer } from '@webb-tools/sdk-mixer';
 
-async function bulletProffMixer() {
-  /*
-   *
-   * */
+// @ts-ignore
+import Worker from './mixer.worker';
+
+const ENDPOINT = 'ws://localhost:9944';
+const apiLogger = LoggerService.get('Api');
+
+async function main() {
+  apiLogger.info('Connecting to ', ENDPOINT);
+  const provider = new WsProvider([ENDPOINT]);
+  const opts = options({ provider });
+  const api = await ApiPromise.create(opts);
+  const result = await api.rpc.system.chain();
+  apiLogger.info('🎉 Connected to ', result.toHuman());
+  const keyring = new Keyring({ type: 'sr25519' });
+  const alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
+
+  const mixer = await Mixer.init(new Worker());
+  const [note, leaf] = await mixer.generateNoteAndLeaf({ id: 0, tokenSymbol: 'EDG' });
+  apiLogger.info(`Your Note: ${note.serialize()}`);
+  await api.tx.mixer.deposit(0, [leaf]).signAndSend(alice, async ({ status, dispatchError }) => {
+    // status would still be set, but in the case of error we can shortcut
+    // to just check it (so an error would indicate InBlock or Finalized)
+    if (dispatchError) {
+      if (dispatchError.isModule) {
+        // for module errors, we have the section indexed, lookup
+        const decoded = api.registry.findMetaError(dispatchError.asModule);
+        const { documentation, name, section } = decoded;
+
+        console.log(`${section}.${name}: ${documentation.join(' ')}`);
+      } else {
+        // Other, CannotLookup, BadOrigin, no extra info
+        apiLogger.error(dispatchError.toString());
+      }
+    }
+    if (status.isInBlock || status.isFinalized) {
+      apiLogger.info('Done!');
+      await api.disconnect();
+    }
+  });
 }
 
-bulletProffMixer();
+main().catch(apiLogger.error);
