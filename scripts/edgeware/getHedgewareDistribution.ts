c028e778cabcc5c588081f38aae36ec071106b70
Drew Stone <drewstone329@gmail.com>
2021-06-06 14:42:55 -0400

    Updates to distribution script

--- scripts/edgeware/getHedgewareDistribution.ts
+++ scripts/edgeware/getHedgewareDistribution.ts
@@ -6,6 +6,10 @@ import BN from 'bn.js';
 import BigNumber from 'bignumber.js';
 import fs from 'fs';
 
+// set bignumber configuration for correct calculations
+BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_DOWN });
+BigNumber.config({ DECIMAL_PLACES: 0 });
+
 const ENDPOINT = 'ws://localhost:9944';
 const apiLogger = LoggerService.get('Api');
 const DISTRIBUTION_HASH = '0x7c3220a5589a8cf5300b5531472bafe164e1690c07d05a469b70717fe8584e8a';
@@ -18,31 +22,39 @@ async function main() {
   await api.isReady;
 
   const entries = await api.query.system.account.entriesAt(DISTRIBUTION_HASH);
-  let sum = new BN('0');
   let quadSum = new BN('0');
   // get sums and square root sums
   entries.forEach((entry) => {
     const accountInfo: AccountInfo = entry[1];
     const accountData: AccountData = accountInfo.data;
-    const sqrtOfFree = new BigNumber(accountData.free.toString()).sqrt();
+    // sum free + reserved
+    const accountBalance = new BN(accountData.free.toString()).add(new BN(accountData.reserved.toString()));
+    // take sqrt
+    const sqrtOfBalance = new BigNumber(accountBalance.toString()).sqrt();
     // add values to sum
-    sum = sum.add(accountData.free);
-    quadSum = quadSum.add(new BN(sqrtOfFree.toString()));
+    quadSum = quadSum.add(new BN(sqrtOfBalance.toString()));
   });
 
+  let totalDistributed = new BN('0');
   const distribution = entries.map((entry) => {
     const accountInfo: AccountInfo = entry[1];
     const accountData: AccountData = accountInfo.data;
-    const sqrtOfFree = new BigNumber(accountData.free.toString()).sqrt();
-    const quadraticAllocation = new BN(sqrtOfFree.toString())
-      // multiply by 5,000,000 tokens w/ 18 decimals -> 24 zeros
+    // sum free + reserved
+    const accountBalance = new BN(accountData.free.toString()).add(new BN(accountData.reserved.toString()));
+    // take sqrt
+    const sqrtOfBalance = new BigNumber(accountBalance.toString()).sqrt();
+    // weight properly for distribution
+    const quadraticAllocation = new BN(sqrtOfBalance.toString())
+      // multiply by 2,500,000 tokens w/ 18 decimals -> 24 zeros
       .mul(new BN('2500000000000000000000000'))
       .div(quadSum);
 
+    totalDistributed = totalDistributed.add(quadraticAllocation);
     return [entry[0].toString().substr(entry[0].toString().length - 64), quadraticAllocation.toString()];
   });
 
   fs.writeFileSync('./distribution.json', JSON.stringify({ balances: distribution }, null, 4));
+  console.log('Total distributed', totalDistributed.toString());
   await api.disconnect();
 }
 
