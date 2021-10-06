b135b887b436c7a88fc95332593a71cd427d0e50
ahmed korim <ahmedkorrim@gmail.com>
2021-10-06 19:41:10 +0200

    Fix auto release versioning

--- scripts/webb-build-release.js
+++ scripts/webb-build-release.js
@@ -104,16 +104,16 @@ function gitBump() {
 
   if (tag) {
     // if we have a beta version, just continue the stream of betas
-    execSync('yarn polkadot-dev-version pre');
+    execSync('./update-version prerelease');
   } else if (argv['skip-beta']) {
     // don't allow beta versions
-    execSync('yarn polkadot-dev-version patch');
+    execSync('./update-version patch');
   } else if (patch === '0') {
     // patch is .0, so publish this as an actual release (surely we did our job on beta)
-    execSync('yarn polkadot-dev-version patch');
+    execSync('./update-version patch');
   } else if (patch === '1') {
     // continue with first new minor as beta
-    execSync('yarn polkadot-dev-version pre');
+    execSync('./update-version prerelease');
   } else {
     // manual setting of version, make some changes so we can commit
     fs.appendFileSync(path.join(process.cwd(), '.123trigger'), currentVersion);
