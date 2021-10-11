ed27411ddfc4219270378d672e60041a9e8e6f9f
ahmed korim <ahmedkorrim@gmail.com>
2021-10-11 16:38:53 +0200

    Logs

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -30,13 +30,14 @@ execSync(`yarn version --${type}`);
 
 // yarn workspaces does an OOM, manual looping takes ages
 if (fs.existsSync('packages')) {
+  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
+  console.log(`Updating packages version ${version}`);
   const packages = fs
     .readdirSync('packages')
     .map((dir) => path.join(process.cwd(), 'packages', dir, 'package.json'))
     .filter((pkgPath) => fs.existsSync(pkgPath))
     .map((pkgPath) => [pkgPath, JSON.parse(fs.readFileSync(pkgPath, 'utf8'))]);
   const others = packages.map(([, json]) => json.name);
-  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
   packages.forEach(([pkgPath, json]) => {
     const updated = Object.keys(json).reduce((result, key) => {
       if (key === 'version') {
@@ -55,14 +56,18 @@ if (fs.existsSync('packages')) {
     fs.writeFileSync(pkgPath, `${JSON.stringify(updated, null, 2)}\n`);
   });
 }
+
 if (fs.existsSync('examples')) {
+  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
+
+  console.log(`Updating examples version ${version}`);
+
   const packages = fs
     .readdirSync('examples')
     .map((dir) => path.join(process.cwd(), 'examples', dir, 'package.json'))
     .filter((pkgPath) => fs.existsSync(pkgPath))
     .map((pkgPath) => [pkgPath, JSON.parse(fs.readFileSync(pkgPath, 'utf8'))]);
   const others = packages.map(([, json]) => json.name);
-  const { version } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
   packages.forEach(([pkgPath, json]) => {
     const updated = Object.keys(json).reduce((result, key) => {
       if (key === 'version') {
@@ -85,4 +90,5 @@ try {
   execSync('yarn');
 } catch (e) {
   console.log('! $YARN failed');
+  console.log(e);
 }
