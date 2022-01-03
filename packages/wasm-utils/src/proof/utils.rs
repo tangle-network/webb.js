4a1d18f823835cd66ebd2db15be674de62399ee1
ahmed korim <ahmedkorrim@gmail.com>
2022-01-03 17:47:39 +0200

    :recycle: Move the mixer crickets proving logic to `proof/mixer.rs`

--- packages/wasm-utils/src/proof/utils.rs
+++
@@ -1,7 +0,0 @@
-use ark_ff::PrimeField;
-use arkworks_utils::poseidon::PoseidonParameters;
-use arkworks_utils::utils::common::{setup_params_x5_3, setup_params_x5_5, Curve};
-
-pub fn get_hash_params_x5<T: PrimeField>(curve: Curve) -> (PoseidonParameters<T>, PoseidonParameters<T>) {
-	(setup_params_x5_3::<T>(curve), setup_params_x5_5::<T>(curve))
-}
