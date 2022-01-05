674764fd40b2411bfe138a9dea02a65035a7c753
ahmed korim <ahmedkorrim@gmail.com>
2022-01-05 13:48:52 +0200

    refactor proof files

--- packages/sdk-mixer/src/types.ts
+++
@@ -1,8 +0,0 @@
-export type TokenSymbol = 'EDG' | 'DOT' | 'DEV' | 'WEBB';
-/**
- * The `Scalar` type alias is byte array that holds an integer (little-endian).
- * and with fixed length 32 bytes.
- */
-export type Scalar = Uint8Array;
-
-export type WebbNotePrefix = 'webb.mix';
