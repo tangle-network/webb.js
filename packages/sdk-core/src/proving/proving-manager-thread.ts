68f83906f068520c391e763807843b4526bbd909
ahmed korim <ahmedkorrim@gmail.com>
2022-01-17 15:24:48 +0200

    Fix lints

--- packages/sdk-core/src/proving/proving-manager-thread.ts
+++ packages/sdk-core/src/proving/proving-manager-thread.ts
@@ -1,8 +1,7 @@
-import type {  Leaves, Proof, JsProofInput } from "@webb-tools/wasm-utils";
-import { u8aToHex } from "@polkadot/util";
-import { ProofI } from "@webb-tools/sdk-core/proving/proving-manger";
-import { Note } from "../note";
-
+import type { Leaves, Proof, JsProofInput } from '@webb-tools/wasm-utils';
+import { u8aToHex } from '@polkadot/util';
+import { ProofI } from '@webb-tools/sdk-core/proving/proving-manger';
+import { Note } from '../note';
 
 export type ProvingManagerSetupInput = {
   note: string;
@@ -13,8 +12,8 @@ export type ProvingManagerSetupInput = {
   fee: number;
   refund: number;
   provingKey: Uint8Array;
-  roots?:Leaves;
-  commitment?:string;
+  roots?: Leaves;
+  commitment?: string;
 };
 
 type PMEvents = {
@@ -24,20 +23,21 @@ type PMEvents = {
 
 export class ProvingManagerWrapper {
   constructor() {
-    self.addEventListener("message", async (event) => {
+    self.addEventListener('message', async (event) => {
       const message = event.data as Partial<PMEvents>;
       const key = Object.keys(message)[0] as keyof PMEvents;
       switch (key) {
-        case "proof": {
-          const input = message.proof!;
-          const proof = await this.proof(input);
-          (self as unknown as Worker).postMessage({
-            name: key,
-            data: proof
-          });
-        }
+        case 'proof':
+          {
+            const input = message.proof!;
+            const proof = await this.proof(input);
+            (self as unknown as Worker).postMessage({
+              name: key,
+              data: proof
+            });
+          }
           break;
-        case "destroy":
+        case 'destroy':
           (self as unknown as Worker).terminate();
           break;
       }
@@ -45,15 +45,14 @@ export class ProvingManagerWrapper {
   }
 
   private static get proofBuilder() {
-    return import("@webb-tools/wasm-utils").then((wasm) => {
+    return import('@webb-tools/wasm-utils').then((wasm) => {
       return wasm.ProofInputBuilder;
     });
   }
 
-  private static async generateProof( proofInput: JsProofInput): Promise<Proof> {
-    const wasm = await import("@webb-tools/wasm-utils");
+  private static async generateProof(proofInput: JsProofInput): Promise<Proof> {
+    const wasm = await import('@webb-tools/wasm-utils');
     return wasm.generate_proof_js(proofInput);
-
   }
 
   async proof(pmSetupInput: ProvingManagerSetupInput): Promise<ProofI> {
@@ -61,7 +60,7 @@ export class ProvingManagerWrapper {
     const pm = new Manager();
     const { note } = await Note.deserialize(pmSetupInput.note);
     // TODO: handle the prefix and validation
-/*    switch (note.prefix){
+    /*    switch (note.prefix){
       case "webb.mixer":
         break;
       case "webb.bridge":
@@ -78,7 +77,7 @@ export class ProvingManagerWrapper {
     pm.setLeafIndex(String(pmSetupInput.leafIndex));
     pm.setRefund(String(pmSetupInput.refund));
     pm.setFee(String(pmSetupInput.fee));
-    pm.setPk(u8aToHex(pmSetupInput.provingKey).replace("0x", ""));
+    pm.setPk(u8aToHex(pmSetupInput.provingKey).replace('0x', ''));
     pm.setNote(note);
     const proofInput = pm.build_js();
     const proof = await ProvingManagerWrapper.generateProof(proofInput);
