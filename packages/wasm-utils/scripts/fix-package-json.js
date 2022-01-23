c2299ad82f4100162473c2aeff96e3ab9bc600e9
ahmed korim <ahmedkorrim@gmail.com>
2022-01-23 18:53:39 +0200

    v0.1.4-19

--- packages/wasm-utils/scripts/fix-package-json.js
+++ packages/wasm-utils/scripts/fix-package-json.js
@@ -1,24 +1,31 @@
 const path = require('path');
 const fs = require('fs');
 const buildDir = path.join(__dirname, '..', 'build');
-console.info('Produce a hybrid package');
-console.log('Reading original package json for root');
-const originalPackageJsonPath = path.join(buildDir, 'package.json');
-const originalPackageJson = JSON.parse(fs.readFileSync(originalPackageJsonPath).toString());
-// Adding `node/` to the published files
-const publishPackageJson = {
-  ...originalPackageJson,
-  files: [...originalPackageJson.files, ...originalPackageJson.files.map((i) => `node/${i}`)]
-};
-console.log('writing the updated package.json');
-fs.writeFileSync(originalPackageJsonPath, JSON.stringify(publishPackageJson, null, 2));
-console.info(`deleting  node extra files`);
-console.log(`Root files `, fs.readdirSync(buildDir));
-console.log(`Node files `, fs.readdirSync(path.join(buildDir, 'node')));
+async function main() {
+  console.info('Produce a hybrid package');
+  console.log('Reading original package json for root');
+  const originalPackageJsonPath = path.join(buildDir, 'package.json');
+  const originalPackageJson = JSON.parse(fs.readFileSync(originalPackageJsonPath).toString());
+  // Adding `node/` to the published files
+  const publishPackageJson = {
+    ...originalPackageJson,
+    files: [...originalPackageJson.files, ...originalPackageJson.files.map((i) => `node/${i}`)]
+  };
+  console.log('writing the updated package.json');
+  /// sleep for other fs oprations
+  await new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000);
+  });
+  fs.writeFileSync(originalPackageJsonPath, JSON.stringify(publishPackageJson, null, 2));
+  console.info(`deleting  node extra files`);
+  console.log(`Root files `, fs.readdirSync(buildDir));
+  console.log(`Node files `, fs.readdirSync(path.join(buildDir, 'node')));
 
-try {
-  fs.unlinkSync(path.join(buildDir, 'node', 'package.json'));
-  console.info('Done patching the package');
-} catch (e) {
-  console.info('Done patching the package , node/package.json already removed');
+  try {
+    fs.unlinkSync(path.join(buildDir, 'node', 'package.json'));
+    console.info('Done patching the package');
+  } catch (e) {
+    console.info('Done patching the package , node/package.json already removed');
+  }
 }
+main();
