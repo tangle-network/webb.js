eb5f6363811437b655d3e3b1ca9a790c6da8c04b
Ahmed Korim <ahmedkorrim@gmail.com>
2022-01-31 16:59:51 +0200

    Build wasm-utils for nodejs (#147)

--- packages/wasm-utils/webpack.config.js
+++ packages/wasm-utils/webpack.config.js
@@ -1,27 +1,43 @@
 const path = require('path');
+const fs = require('fs');
 const CopyPlugin = require('copy-webpack-plugin');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
-const build = path.resolve(__dirname, 'build');
-module.exports = {
-  mode: 'production',
-  entry: {
-    utils: './js/utils.js'
-  },
-  output: {
-    path: build,
-    filename: '[name].js'
-  },
-  devServer: {
-    contentBase: build
-  },
-  plugins: [
-    new CopyPlugin([path.resolve(__dirname, 'public'), path.resolve(__dirname, 'package.json')]),
-    new WasmPackPlugin({
-      extraArgs: '--scope webb-tools',
-      crateDirectory: __dirname,
-      outDir: build,
-      outName: 'wasm-utils'
-    })
-  ]
+module.exports = function () {
+  const nodeBuild = path.join(__dirname, 'build', 'njs');
+  const build = path.join(__dirname, 'build');
+
+  try {
+    console.log(`Pre build dir`, fs.readdirSync(path.join(__dirname, 'build')));
+  } catch (_) {}
+
+  const args = '--scope webb-tools';
+  return {
+    mode: 'production',
+    entry: {
+      utils: './js/utils.js'
+    },
+    output: {
+      path: build,
+      filename: '[name].js'
+    },
+    devServer: {
+      contentBase: build
+    },
+    plugins: [
+      new CopyPlugin([path.resolve(__dirname, 'public'), path.resolve(__dirname, 'package.json')]),
+      new WasmPackPlugin({
+        extraArgs: args,
+        crateDirectory: __dirname,
+        outDir: build,
+        outName: 'wasm-utils'
+      }),
+      new WasmPackPlugin({
+        extraArgs: `${args} --target nodejs`,
+        crateDirectory: __dirname,
+        outDir: nodeBuild,
+        outName: 'wasm-utils'
+      })
+    ]
+  };
 };
