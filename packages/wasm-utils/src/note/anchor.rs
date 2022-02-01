94cf7ca4d48450e32b11e53778849fdd3725cdbb
Ahmed Korim <ahmedkorrim@gmail.com>
2022-02-01 11:50:13 +0200

    Update packages/wasm-utils/src/note/anchor.rs
    Co-authored-by: Filip Lazovic <lazoviccorp@gmail.com>

--- packages/wasm-utils/src/note/anchor.rs
+++ packages/wasm-utils/src/note/anchor.rs
@@ -19,7 +19,7 @@ pub fn generate_secrets(
 		(Curve::Bn254, 5, 4) => setup_leaf_x5_4::<Bn254Fr, _>(ArkworksCurve::Bn254, chain_id, rng),
 		_ => {
 			let message = format!(
-				"No Anchor secrets setup available for curve {}, exponentiation {} , and width {}",
+				"No Anchor secrets setup available for curve {}, exponentiation {}, and width {}",
 				curve, exponentiation, width
 			);
 			return Err(OperationError::new_with_message(OpStatusCode::SecretGenFailed, message));
