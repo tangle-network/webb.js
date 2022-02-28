b5ca9ac0cc9ebaa3d3e1ad52a7923fc66907f70c
oayomide <onigbindeayomide@gmail.com>
2022-02-28 13:54:22 +0100

    update

--- packages/sdk-core/src/proof.spec.ts
+++ packages/sdk-core/src/proof.spec.ts
@@ -2,32 +2,46 @@ import type { ProvingManagerSetupInput } from './proving';
 import { ProvingManagerWrapper } from './proving'
 import path from 'path'
 import { readFile } from 'fs/promises'
-// import { ApiPromise, WsProvider } from '@polkadot/api'
+import { u8aToString } from '@polkadot/util'
 import { Note } from '.';
+import { OperationError } from '@webb-tools/wasm-utils/build/njs';
 
 describe('Proof tests', () => {
-    it('Should generate a new proof', async () => {
-        // const wsProvider = new WsProvider('ws://127.0.0.1:9944');
-        // const _api = await ApiPromise.create({ provider: wsProvider })
-        const pkPath = path.join(process.cwd(), 'fixtures', 'proving_key.bin');
-        const pk = await readFile(pkPath);
+    it('Should prove an anchor note', async () => {
+        try {
+            const pkPath = path.join(process.cwd(), 'fixtures', 'proving_key.bin');
+            const pk = await readFile(pkPath);
 
-        const note = await Note.deserialize('webb://v2:anchor/1099511627780:1099511627780/0x4e7D4BEe028655F2865d9D147cF7B609c516d39C:0x4e7D4BEe028655F2865d9D147cF7B609c516d39C/010000000004:00defe0ff44373b7c9c892a3fd3c72af100b35b7b319686aac357bb41d52eb2d:00b62b13d3af6d4f585cef8775e0c1bc11f77ff545c468b3bed00024d342815c/?curve=Bn254&width=3&exp=5&hf=Poseidon&backend=Circom&token=webbWETH&denom=18&amount=0.1')
-        const noteString = note.serialize();
-        const newInput: ProvingManagerSetupInput = {
-            fee: 10,
-            leafIndex: 1,
-            leaves: [note.getLeaf()],
-            note: noteString,
-            refund: 0.1,
-            provingKey: pk,
-            relayer: '0x9fe76e5833db2ba72b3a3b4b297ce36279bee41f7fc403d057dbe664d3c2b96b',
-            recipient: '0x9fe76e5833db2ba72b3a3b4b297ce36279bee41f7fc403d057dbe664d3c2b96b',
-            commitment: '10',
+
+            const note = await Note.deserialize('webb://v2:anchor/1099511627780:1099511627780/0x4e7D4BEe028655F2865d9D147cF7B609c516d39C:0x4e7D4BEe028655F2865d9D147cF7B609c516d39C/010000000004:00defe0ff44373b7c9c892a3fd3c72af100b35b7b319686aac357bb41d52eb2d:00b62b13d3af6d4f585cef8775e0c1bc11f77ff545c468b3bed00024d342815c/?curve=Bn254&width=4&exp=5&hf=Poseidon&backend=Arkworks&token=webbWETH&denom=18&amount=0.1')
+            console.log(note.serialize())
+            const noteString = note.serialize();
+            const depositeNote = await note.toDepositNote()
+            const newInput: ProvingManagerSetupInput = {
+                fee: 10,
+                leafIndex: 0,
+                leaves: [note.getLeaf()],
+                note: noteString,
+                refund: 0.1,
+                provingKey: pk,
+                relayer: depositeNote.sourceChainId,
+                recipient: depositeNote.targetChainId,
+                commitment: u8aToString(depositeNote.getLeafCommitment()),
+            }
+
+
+            const Proof = new ProvingManagerWrapper()
+            // it seems to fail here.
+            const proof = await Proof.proof(newInput)
+            expect(proof.nullifierHash).toContain("0x")
+        } catch (e) {
+            if (e instanceof OperationError) {
+                console.log({
+                    code: e.code,
+                    errorMessage: e.error_message,
+                    data: e.data
+                })
+            }
         }
-        const Proof = new ProvingManagerWrapper()
-        const proof = await Proof.proof(newInput)
-        // dont know how proof looks like yet :)
-        expect(proof.nullifierHash).toContain("0x")
     })
 })
