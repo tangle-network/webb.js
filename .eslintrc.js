063650365b8fbfef60b8e59436bd31ea19216ca4
nepoche <nepoche@yahoo.com>
2022-03-17 13:23:52 -0400

    Overwrite a bunch of ts lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,16 @@ module.exports = {
       { pattern: ' Copyright 2022 @webb-tools/' },
       ' SPDX-License-Identifier: Apache-2.0'
     ], 2],
+    // disable some rules brought in by polkadot/dev
     'no-useless-constructor': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-argument': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
+    '@typescript-eslint/restrict-template-expressions': 0,
+    '@typescript-eslint/unbound-method': 0,
+    '@typescript-eslint/await-thenable': 0,
+    '@typescript-eslint/no-misused-promises': 0
   }
 };
