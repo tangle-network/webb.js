00c9b0fbffd3b92f1db709aefaed68dd748f7538
ahmed korim <ahmedkorrim@gmail.com>
2022-05-18 13:09:32 +0200

    Adding more test with different inputs for the VAnchor

--- packages/sdk-core/src/__test__/proving.spec.ts
+++ packages/sdk-core/src/__test__/proving.spec.ts
@@ -41,10 +41,58 @@ function generateVAnchorNote (
   return note;
 }
 
-describe.only('Proving manager', function () {
-  this.timeout(120_000);
+describe('Proving manager VAnchor', function () {
+  this.timeout(720_000);
+  it('Should proof js for VAnchor with one inputs', async () => {
+    console.log('===> Generating keys for vanchor');
+    const keys = setupKeys('vanchor');
 
-  it('Should proof js for VAnchor', async () => {
+    console.log('===> Keys are generated');
+    const vanchorNote1 = generateVAnchorNote(20, 0, 0, 0);
+
+    console.log('===> Generated vanchor notes');
+
+    const publicAmount = 10;
+    const outputAmount = String(15);
+    const outputChainId = BigInt(0);
+
+    const leaf1 = vanchorNote1.getLeafCommitment();
+
+    console.log('===> Tree setup with leaves');
+
+    const tree = new MTBn254X5([leaf1], '0');
+    const root = `0x${tree.root}`;
+    const rootsSet = [hexToU8a(root), hexToU8a(root)];
+    const leavesMap: any = {};
+
+    leavesMap[0] = [leaf1];
+    const externalDataHash = '10101010101010101010';
+
+    const outputConfig1 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+    const outputConfig2 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+
+    const provingManager = new ProvingManagerWrapper('direct-call');
+    const setup: ProvingManagerSetupInput<'vanchor'> = {
+      chainId: '0',
+      externalDataHash,
+      indices: [0],
+      inputNotes: [vanchorNote1.serialize()],
+      leavesMap: leavesMap,
+      outputConfigs: [outputConfig1, outputConfig2],
+      provingKey: keys.pk,
+      publicAmount: String(publicAmount),
+      roots: rootsSet
+
+    };
+
+    const data = await provingManager.proof('vanchor', setup);
+
+    const isValidProof = verify_js_proof(data.proof, data.publicInputs, u8aToHex(keys.vk).replace('0x', ''), 'Bn254');
+
+    expect(isValidProof).to.deep.equal(true);
+  });
+
+  it('Should proof js for VAnchor with two inputs', async () => {
     console.log('===> Generating keys for vanchor');
     const keys = setupKeys('vanchor');
 
@@ -80,6 +128,59 @@ describe.only('Proving manager', function () {
       externalDataHash,
       indices: [0, 1],
       inputNotes: [vanchorNote1.serialize(), vanchorNote2.serialize()],
+      leavesMap: leavesMap,
+      outputConfigs: [outputConfig1, outputConfig2],
+      provingKey: keys.pk,
+      publicAmount: String(publicAmount),
+      roots: rootsSet
+
+    };
+
+    const data = await provingManager.proof('vanchor', setup);
+
+    const isValidProof = verify_js_proof(data.proof, data.publicInputs, u8aToHex(keys.vk).replace('0x', ''), 'Bn254');
+
+    expect(isValidProof).to.deep.equal(true);
+  });
+
+  it('Should proof js for VAnchor with three inputs', async () => {
+    console.log('===> Generating keys for vanchor');
+    const keys = setupKeys('vanchor');
+
+    console.log('===> Keys are generated');
+    const vanchorNote1 = generateVAnchorNote(10, 0, 0, 0);
+    const vanchorNote2 = generateVAnchorNote(10, 0, 0, 1);
+    const vanchorNote3 = generateVAnchorNote(10, 0, 0, 2);
+
+    console.log('===> Generated vanchor notes');
+
+    const publicAmount = 0;
+    const outputAmount = String(15);
+    const outputChainId = BigInt(0);
+
+    const leaf1 = vanchorNote1.getLeafCommitment();
+    const leaf2 = vanchorNote2.getLeafCommitment();
+    const leaf3 = vanchorNote2.getLeafCommitment();
+
+    console.log('===> Tree setup with leaves');
+
+    const tree = new MTBn254X5([leaf1, leaf2, leaf3], '0');
+    const root = `0x${tree.root}`;
+    const rootsSet = [hexToU8a(root), hexToU8a(root)];
+    const leavesMap: any = {};
+
+    leavesMap[0] = [leaf1, leaf2, leaf3];
+    const externalDataHash = '10101010101010101010';
+
+    const outputConfig1 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+    const outputConfig2 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+
+    const provingManager = new ProvingManagerWrapper('direct-call');
+    const setup: ProvingManagerSetupInput<'vanchor'> = {
+      chainId: '0',
+      externalDataHash,
+      indices: [0, 1, 2],
+      inputNotes: [vanchorNote1.serialize(), vanchorNote2.serialize(), vanchorNote3.serialize()],
       leavesMap: leavesMap,
       outputConfigs: [outputConfig1, outputConfig2],
       provingKey: keys.pk,
