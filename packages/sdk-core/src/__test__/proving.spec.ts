2001a17fb671a33285bee47a124ee826be23ef04
ahmed korim <ahmedkorrim@gmail.com>
2022-05-18 15:54:43 +0200

    Restore 3 inputs unit test for VAnchor

--- packages/sdk-core/src/__test__/proving.spec.ts
+++ packages/sdk-core/src/__test__/proving.spec.ts
@@ -138,7 +138,60 @@ describe('Proving manager VAnchor', function () {
     expect(isValidProof).to.deep.equal(true);
   });
 
-  it.only('Should proof js for VAnchor with 16 inputs', async () => {
+  it('Should proof js for VAnchor with three inputs', async () => {
+    console.log('===> Generating keys for vanchor');
+    const keys = setupKeys('vanchor');
+
+    console.log('===> Keys are generated');
+    const vanchorNote1 = generateVAnchorNote(10, 0, 0, 0);
+    const vanchorNote2 = generateVAnchorNote(10, 0, 0, 1);
+    const vanchorNote3 = generateVAnchorNote(10, 0, 0, 2);
+
+    console.log('===> Generated vanchor notes');
+
+    const publicAmount = 0;
+    const outputAmount = String(15);
+    const outputChainId = BigInt(0);
+
+    const leaf1 = vanchorNote1.getLeafCommitment();
+    const leaf2 = vanchorNote2.getLeafCommitment();
+    const leaf3 = vanchorNote2.getLeafCommitment();
+
+    console.log('===> Tree setup with leaves');
+
+    const tree = new MTBn254X5([leaf1, leaf2, leaf3], '0');
+    const root = `0x${tree.root}`;
+    const rootsSet = [hexToU8a(root), hexToU8a(root)];
+    const leavesMap: any = {};
+
+    leavesMap[0] = [leaf1, leaf2, leaf3];
+    const externalDataHash = '10101010101010101010';
+
+    const outputConfig1 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+    const outputConfig2 = new OutputUtxoConfig(outputAmount, undefined, outputChainId);
+
+    const provingManager = new ProvingManagerWrapper('direct-call');
+    const setup: ProvingManagerSetupInput<'vanchor'> = {
+      chainId: '0',
+      externalDataHash,
+      indices: [0, 1, 2],
+      inputNotes: [vanchorNote1.serialize(), vanchorNote2.serialize(), vanchorNote3.serialize()],
+      leavesMap: leavesMap,
+      outputConfigs: [outputConfig1, outputConfig2],
+      provingKey: keys.pk,
+      publicAmount: String(publicAmount),
+      roots: rootsSet
+
+    };
+
+    const data = await provingManager.proof('vanchor', setup);
+
+    const isValidProof = verify_js_proof(data.proof, data.publicInputs, u8aToHex(keys.vk).replace('0x', ''), 'Bn254');
+
+    expect(isValidProof).to.deep.equal(true);
+  });
+
+  it('Should proof js for VAnchor with 16 inputs', async () => {
     const keys = setupKeys('vanchor');
 
     const notes = Array(16).fill(0).map((_, index) => generateVAnchorNote(10, 0, 0, index));
