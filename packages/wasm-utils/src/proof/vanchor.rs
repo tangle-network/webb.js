3799ac64b035624a9ff7b50051d2de18ea949a7d
ahmed korim <ahmedkorrim@gmail.com>
2022-05-19 12:44:13 +0200

    make the VAncor api more strict with inputs

--- packages/wasm-utils/src/proof/vanchor.rs
+++ packages/wasm-utils/src/proof/vanchor.rs
@@ -12,6 +12,9 @@ use crate::types::{Backend, Curve, HashFunction, NoteProtocol, NoteVersion, OpSt
 use crate::utxo::JsUtxo;
 use crate::{VAnchorR1CSProverBn254_30_2_16_2, VAnchorR1CSProverBn254_30_2_2_2, DEFAULT_LEAF};
 
+const SUPPORTED_INPUT_COUNT: [usize; 2] = [2, 16];
+const SUPPORTED_ANCHOR_COUNT: [usize; 2] = [2, 16];
+
 fn get_output_notes(
 	anchor_proof_input: &VAnchorProofPayload,
 	output_config: [JsUtxo; 2],
@@ -66,109 +69,124 @@ pub fn create_proof(vanchor_proof_input: VAnchorProofPayload, rng: &mut OsRng) -
 		ext_data_hash,
 	} = vanchor_proof_input.clone();
 
-	// Prepare in UTXOs
-	let in_utxos: Vec<JsUtxo> = match secret.len() {
-		2 | 16 => secret,
-		length if length < 16 && length > 2 => {
-			let mut utxos: Vec<JsUtxo> = secret;
-			let utxo = VAnchorR1CSProverBn254_30_2_2_2::create_random_utxo(ArkCurve::Bn254, 0, 0, Some(0), &mut OsRng)
-				.unwrap();
-			// Create empty UTXOs to fill the list with  16 UTXOs
-			let js_utxo = JsUtxo::new_from_bn254_utxo(utxo);
-			loop {
-				if utxos.len() == 16 {
-					break;
-				}
-				utxos.push(js_utxo.clone())
-			}
-			utxos
-		}
-		1 => {
-			let utxo = VAnchorR1CSProverBn254_30_2_2_2::create_random_utxo(ArkCurve::Bn254, 0, 0, Some(0), &mut OsRng)
-				.unwrap();
-			let utxo = JsUtxo::new_from_bn254_utxo(utxo);
-			vec![secret[0].clone(), utxo]
-		}
-		length => {
-			return Err(OperationError::new_with_message(
-				OpStatusCode::InvalidNoteLength,
-				format!("The number of supplied notes {} can't be used for prooving ", length),
-			));
-		}
+	// Insure UTXO set has the required/supported input count
+	let in_utxos: Vec<JsUtxo> = if SUPPORTED_INPUT_COUNT.contains(&secret.len()) {
+		secret
+	} else {
+		let message = format!(
+			"Input  set has {} UTXOs while the supported set length should be one of {:?}",
+			&secret.len(),
+			&SUPPORTED_INPUT_COUNT,
+		);
+		return Err(OperationError::new_with_message(
+			OpStatusCode::InvalidProofParameters,
+			message,
+		));
 	};
-	let utxo_o_1 = VAnchorR1CSProverBn254_30_2_2_2::new_utxo(
-		ArkCurve::Bn254,
-		output_config[0].chain_id,
-		Bn254Fr::from(output_config[0].amount),
-		None,
-		None,
-		None,
-		&mut OsRng,
-	)
-	.unwrap();
-	let utxo_o_2 = VAnchorR1CSProverBn254_30_2_2_2::new_utxo(
-		ArkCurve::Bn254,
-		output_config[1].chain_id,
-		Bn254Fr::from(output_config[1].amount),
-		None,
-		None,
-		None,
-		&mut OsRng,
-	)
-	.unwrap();
-	let utxos_out = [utxo_o_1.clone(), utxo_o_2.clone()];
-	// output payload utxos
-	let input_utxos = in_utxos.clone();
-	let output_notes = get_output_notes(&vanchor_proof_input, [
-		JsUtxo::new_from_bn254_utxo(utxo_o_1),
-		JsUtxo::new_from_bn254_utxo(utxo_o_2),
-	])?;
-	let vanchor_proof = match (backend, curve, exponentiation, width, in_utxos.len()) {
-		(Backend::Arkworks, Curve::Bn254, 5, 5, 2) => {
-			let utxos_in: [Utxo<Bn254Fr>; 2] = [in_utxos[0].get_bn254_utxo()?, in_utxos[1].get_bn254_utxo()?];
-			let indices = indices.try_into().map_err(|_| OpStatusCode::InvalidIndices)?;
-			let roots = roots.try_into().map_err(|_| OpStatusCode::InvalidRoots)?;
-			VAnchorR1CSProverBn254_30_2_2_2::create_proof(
+	// Insure the length of the indices
+	if &indices.len() != &secret.len() {
+		let message = format!(
+      "Indices Array don't match with the Input size , supplied {} indices while there are {} utxos in the input ",
+      &indices.len(),
+      &secret.len(),
+    );
+		return Err(OperationError::new_with_message(
+			OpStatusCode::InvalidProofParameters,
+			message,
+		));
+	}
+	// Insure the Anchor count is supported
+	if !SUPPORTED_ANCHOR_COUNT.contains(&roots.len()) {
+		let message = format!(
+			"Input  set has {} roots while the supported set length should be one of {:?}",
+			&roots.len(),
+			&SUPPORTED_ANCHOR_COUNT,
+		);
+		return Err(OperationError::new_with_message(
+			OpStatusCode::InvalidProofParameters,
+			message,
+		));
+	};
+	// Initialize the output notes vec
+	let mut output_notes = vec![];
+	let proof = match (backend, curve, roots.len()) {
+		(Backend::Arkworks, Curve::Bn254, 2) => {
+			let utxo_o_1 = VAnchorR1CSProverBn254_30_2_2_2::create_random_utxo(
 				ArkCurve::Bn254,
-				chain_id,
-				public_amount,
-				ext_data_hash,
-				roots,
-				indices,
-				leaves,
-				utxos_in,
-				utxos_out,
-				pk,
-				DEFAULT_LEAF,
-				rng,
+				output_config[0].chain_id,
+				output_config[0].amount,
+				None,
 			)
-		}
-		(Backend::Arkworks, Curve::Bn254, 5, 5, 16) => {
-			let in_utxos = in_utxos
-				.into_iter()
-				.map(|utxo| utxo.get_bn254_utxo())
-				.collect::<Result<Vec<_>, _>>()?;
-			let boxed_slice = in_utxos.into_boxed_slice();
-			let boxed_array: Box<[Utxo<Bn254Fr>; 16]> =
-				boxed_slice.try_into().map_err(|_| OpStatusCode::InvalidNoteSecrets)?;
-			let utxos_slice = *boxed_array;
-			let indices = indices.try_into().map_err(|_| OpStatusCode::InvalidIndices)?;
-			let roots = roots.try_into().map_err(|_| OpStatusCode::InvalidRoots)?;
+			.unwrap();
+			let utxo_o_2 = VAnchorR1CSProverBn254_30_2_2_2::create_random_utxo(
+				ArkCurve::Bn254,
+				output_config[1].chain_id,
+				output_config[1].amount,
+				None,
+			)
+			.unwrap();
+			let utxos_out = [utxo_o_1.clone(), utxo_o_2.clone()];
+			output_notes = get_output_notes(&vanchor_proof_input, [
+				JsUtxo::new_from_bn254_utxo(utxo_o_1),
+				JsUtxo::new_from_bn254_utxo(utxo_o_2),
+			])?
+			.to_vec();
+			match (exponentiation, width, in_utxos.len()) {
+				(5, 5, 2) => {
+					let utxos_in: [Utxo<Bn254Fr>; 2] = [in_utxos[0].get_bn254_utxo()?, in_utxos[1].get_bn254_utxo()?];
+					let indices = indices.try_into().map_err(|_| OpStatusCode::InvalidIndices)?;
+					let roots = roots.try_into().map_err(|_| OpStatusCode::InvalidRoots)?;
+					VAnchorR1CSProverBn254_30_2_2_2::create_proof(
+						ArkCurve::Bn254,
+						chain_id,
+						public_amount,
+						ext_data_hash,
+						roots,
+						indices,
+						leaves,
+						utxos_in,
+						utxos_out,
+						pk,
+						DEFAULT_LEAF,
+						rng,
+					)
+				}
+				(5, 5, 16) => {
+					let in_utxos = in_utxos
+						.into_iter()
+						.map(|utxo| utxo.get_bn254_utxo())
+						.collect::<Result<Vec<_>, _>>()?;
+					let utxos_slice = in_utxos.try_into().map_err(|_| OpStatusCode::InvalidNoteSecrets)?;
+					let indices = indices.try_into().map_err(|_| OpStatusCode::InvalidIndices)?;
+					let roots = roots.try_into().map_err(|_| OpStatusCode::InvalidRoots)?;
 
-			VAnchorR1CSProverBn254_30_2_16_2::create_proof(
-				ArkCurve::Bn254,
-				chain_id,
-				public_amount,
-				ext_data_hash,
-				roots,
-				indices,
-				leaves,
-				utxos_slice,
-				utxos_out,
-				pk,
-				DEFAULT_LEAF,
-				rng,
-			)
+					VAnchorR1CSProverBn254_30_2_16_2::create_proof(
+						ArkCurve::Bn254,
+						chain_id,
+						public_amount,
+						ext_data_hash,
+						roots,
+						indices,
+						leaves,
+						utxos_slice,
+						utxos_out,
+						pk,
+						DEFAULT_LEAF,
+						rng,
+					)
+				}
+				_ => {
+					let message = format!(
+						"The proofing setup for backend {} curve {} width {} exp {} input size {} isn't implemented!",
+						backend,
+						curve,
+						width,
+						exponentiation,
+						&in_utxos.len(),
+					);
+					Err(Error::from(message))
+				}
+			}
 		}
 		_ => {
 			let message = format!(
@@ -177,7 +195,7 @@ pub fn create_proof(vanchor_proof_input: VAnchorProofPayload, rng: &mut OsRng) -
 				curve,
 				width,
 				exponentiation,
-				in_utxos.len(),
+				&in_utxos.len(),
 			);
 			Err(Error::from(message))
 		}
@@ -187,8 +205,8 @@ pub fn create_proof(vanchor_proof_input: VAnchorProofPayload, rng: &mut OsRng) -
 		OperationError::new_with_message(OpStatusCode::InvalidProofParameters, message)
 	})?;
 	Ok(VAnchorProof {
-		proof: vanchor_proof.proof,
-		public_inputs: vanchor_proof.public_inputs_raw,
+		proof: proof.proof,
+		public_inputs: proof.public_inputs_raw,
 		output_notes: output_notes.to_vec(),
 		input_utxos,
 	})
