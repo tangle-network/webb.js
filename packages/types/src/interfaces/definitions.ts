0256c0bf51170655bfcd076a613dab8fa8670b52
Dustin Brickwood <dustinbrickwood204@gmail.com>
2022-05-31 13:41:00 -0500

    chore: make requested changes

--- packages/types/src/interfaces/definitions.ts
+++ packages/types/src/interfaces/definitions.ts
@@ -1,8 +1,4 @@
-// Copyright (C) 2020-2022 Acala Foundation.
-// SPDX-License-Identifier: Apache-2.0
-
 // Copyright 2022 Webb Technologies Inc.
 // SPDX-License-Identifier: Apache-2.0
-// This file has been modified by Webb Technologies Inc.
 
 export { default as merkle } from './merkle/definitions';
