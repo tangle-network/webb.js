e39a8eed05e06fcb3f822082c29b18cca11c6aae
ahmed korim <ahmedkorrim@gmail.com>
2022-08-09 16:28:50 +0200

    Change chain id bytes to be BE

--- packages/wasm-utils/src/utxo.rs
+++ packages/wasm-utils/src/utxo.rs
@@ -93,7 +93,7 @@ impl JsUtxo {
 
 	pub fn get_chain_id_bytes(&self) -> Vec<u8> {
 		match &self.inner {
-			JsUtxoInner::Bn254(bn254_utxo) => bn254_utxo.chain_id.into_repr().to_bytes_le(),
+			JsUtxoInner::Bn254(bn254_utxo) => bn254_utxo.chain_id.into_repr().to_bytes_be(),
 		}
 	}
 
