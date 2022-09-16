f03d8af935e905eb971fdd65d9e8450f0b214211
Damilare <dharjeezy@gmail.com>
2022-09-16 12:42:03 +0100

    unskip

--- tests/utils/backend-utils.ts
+++ tests/utils/backend-utils.ts
@@ -7,27 +7,23 @@ const usageMode: UsageMode = isCi
   : {
     mode: 'host',
     nodePath: path.resolve(
-      '../protocol-substrate/target/release/webb-standalone-node'
+      '../../protocol-substrate/target/release/webb-standalone-node'
     ),
   };
 
 export async function startProtocolSubstrateNodes(): Promise<LocalProtocolSubstrate[]> {
-  const aliceManualPorts = { ws: 9944, http: 9933, p2p: 30333 };
-  const bobManualPorts = { ws: 9945, http: 9934, p2p: 30334 };
   let aliceNode = await LocalProtocolSubstrate.start({
     name: 'substrate-alice',
     authority: 'alice',
     usageMode,
-    ports: aliceManualPorts,
-    isManual: true
+    ports: 'auto'
   });
 
   let bobNode = await LocalProtocolSubstrate.start({
     name: 'substrate-bob',
     authority: 'bob',
     usageMode,
-    ports: bobManualPorts,
-    isManual: true
+    ports: 'auto'
   });
 
   return [aliceNode, bobNode];
